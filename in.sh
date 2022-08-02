00093ba5798ec357dfe96eda7ad707f2ad176c4d
Royalboy2000 <88582814+Royalboy2000@users.noreply.github.com>
2022-08-02 14:23:48 +0300

    Add files via upload

---
+++ in.sh
@@ -0,0 +1,20 @@
+echo " _______  _____  _______ _______ ______  __   __     
+ |______ |_____] |______ |______ |     \   \_/       
+ ______| |       |______ |______ |_____/    |        
+                                                      "
+echo         "TikTok = royal._.boy7"
+echo "telegram group = https://t.me/t3amlevine654"
+echo      "instagram = s.a.m.i.r_012" 
+
+install=1
+ echo "do you want to use termux on your pc?"
+pkg update && pkg upgrade -y
+pkg install openssh nmap -y
+
+sshd
+
+ifconfig
+
+nmap localhost
+NAME= whoami
+echo "COPY YOUR IP ADRESS THEN THE PORT $NAME "
